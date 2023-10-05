b38a643cb1d36493edc120c5efc2b4e6e5a05f6a
Poojan Bhatt <pdbhatt010@gmail.com>
2023-10-05 11:39:59 +0530

    updated perform operation lambda

--- src/handlers/performOperation.ts
+++ src/handlers/performOperation.ts
@@ -1,7 +1,34 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { UpdateCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
 export const handler = async (event: any): Promise<any> => {
   try {
-    console.log(event);
-    return event;
+    const client = new DynamoDBClient({});
+    const docClient = DynamoDBDocumentClient.from(client);
+
+    const parsedDescription = parseFloat(event.description);
+
+    const newDescription = !isNaN(parsedDescription)
+      ? (parsedDescription + 1).toString()
+      : "1";
+
+    const command = new UpdateCommand({
+      TableName: process.env.TABLE_NAME,
+      Key: {
+        taskId: event.taskId,
+      },
+      UpdateExpression: "set status = :status",
+      ExpressionAttributeValues: {
+        ":status": newDescription,
+      },
+      ReturnValues: "ALL_NEW",
+    });
+
+    const tableResponse = await docClient.send(command);
+
+    return {
+      ...tableResponse,
+    };
   } catch (error) {
     console.log(error);
   }
