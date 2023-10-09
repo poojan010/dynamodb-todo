9a86bc4af21a1a846266bde26aec90700f7660a6
Poojan Bhatt <pdbhatt010@gmail.com>
2023-10-09 15:30:12 +0530

    updating hyphen with underscore

--- src/step-functions/index.ts
+++ src/step-functions/index.ts
@@ -45,7 +45,7 @@ export const MyStateMachine: AwsStateMachines = {
                   // eslint-disable-next-line max-len
                   "Fn::Sub":
                     // eslint-disable-next-line max-len
-                    "arn:aws:lambda:${AWS::Region}:${env.AWS_ACCOUNT_ID}:function/performOperation-${$.Map.Item.taskId}",
+                    "arn:aws:lambda:${AWS::Region}:${env.AWS_ACCOUNT_ID}:function/performOperation_${$.Map.Item.taskId}",
                 },
                 End: true,
               },
