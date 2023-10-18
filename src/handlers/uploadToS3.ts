fadfe18a644ba5421903484525ca7480f8bc2a23
Poojan Bhatt <pdbhatt010@gmail.com>
2023-10-18 15:44:33 +0530

    testing

--- src/handlers/uploadToS3.ts
+++ src/handlers/uploadToS3.ts
@@ -1,12 +1,12 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+// import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { APIGatewayEvent, APIGatewayProxyHandler } from "aws-lambda";
-import { ScanCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+// import { ScanCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 import { response } from "@lib/api-gateway";
-import { readFile } from "node:fs/promises";
+// import { readFile } from "node:fs/promises";
 
-import { uploadFile } from "@lib/s3";
-import { writeCSVRecords } from "src/utils/csvHandler";
+import { getFileStream } from "@lib/s3";
+import { extractCSVRecords } from "src/utils/csvHandler";
 import { BucketNames, tempEntriesFile } from "src/resources/constants";
 
 export const handler: APIGatewayProxyHandler = async (
@@ -15,23 +15,28 @@ export const handler: APIGatewayProxyHandler = async (
   try {
     console.log(event);
 
-    const client = new DynamoDBClient({});
-    const docClient = DynamoDBDocumentClient.from(client);
-    const command = new ScanCommand({
-      ProjectionExpression: "projectID, taskId, #d, description, fileLocation",
-      ExpressionAttributeNames: { "#d": "date" },
-      TableName: process.env.TABLE_NAME,
-    });
+    const data = await getFileStream(BucketNames.MyS3Bucket, tempEntriesFile);
+    const records = await extractCSVRecords(data);
 
-    const tableResponse = await docClient.send(command);
+    // const client = new DynamoDBClient({});
+    // const docClient = DynamoDBDocumentClient.from(client);
+    // const command = new ScanCommand({
+    // eslint-disable-next-line max-len
+    //   ProjectionExpression: "projectID, taskId, #d, description, fileLocation",
+    //   ExpressionAttributeNames: { "#d": "date" },
+    //   TableName: process.env.TABLE_NAME,
+    // });
 
-    const csvPath = await writeCSVRecords(tableResponse.Items);
-    const bufferData = await readFile(csvPath);
+    // const tableResponse = await docClient.send(command);
 
-    await uploadFile(BucketNames.MyS3Bucket, tempEntriesFile, bufferData);
+    // const csvPath = await writeCSVRecords(tableResponse.Items);
+    // const bufferData = await readFile(csvPath);
+
+    // await uploadFile(BucketNames.MyS3Bucket, tempEntriesFile, bufferData);
 
     return response(200, {
       message: "Succcess",
+      records,
     });
   } catch (error) {
     return response(400, {
